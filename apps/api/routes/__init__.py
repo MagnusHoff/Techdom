feb6cddc08859cc81f936d756bda5b868b08b587
Magnus <magnushoff@outlook.com>
2025-10-09 22:06:45 +0200

    bugfix

--- apps/api/routes/__init__.py
+++ apps/api/routes/__init__.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 from .auth import router as auth_router
 from .analyses import router as analyses_router
-from .stripe import router as stripe_router
+from .stripe_webhook import router as stripe_router
 
 __all__ = ["auth_router", "analyses_router", "stripe_router"]
